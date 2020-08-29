d814a6bc8bf95d014e8e57841a2e9af1710a041b
Iulian Gabriel Radu <iulian.radu67@gmail.com>
2020-08-29 22:25:10 +0300

    simplify error module by using thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,64 +1,17 @@
-use std::error::Error;
-use std::fmt;
-use std::fmt::{Debug, Formatter};
+use thiserror::Error;
 
-use data_encoding::DecodeError;
-
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum LorError {
-    Decode(data_encoding::DecodeError),
-    VarintDecode(std::io::Error),
-    InvalidCardCode(std::num::ParseIntError),
+    #[error("decode")]
+    Decode(#[from] data_encoding::DecodeError),
+    #[error("varint decode")]
+    VarintDecode(#[from] std::io::Error),
+    #[error("invalid card code")]
+    InvalidCardCode(#[from] std::num::ParseIntError),
+    #[error("invalid card")]
     InvalidCard,
+    #[error("invalid deck")]
     InvalidDeck,
+    #[error("version error")]
     Version,
-}
-
-impl fmt::Display for LorError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match *self {
-            LorError::Decode(ref err) => <DecodeError as fmt::Display>::fmt(err, f),
-            LorError::VarintDecode(ref err) => <std::io::Error as fmt::Display>::fmt(err, f),
-            LorError::InvalidCardCode(ref err) => {
-                <std::num::ParseIntError as fmt::Display>::fmt(err, f)
-            }
-            LorError::InvalidCard => write!(f, "Invalid card."),
-            LorError::InvalidDeck => write!(f, "Invalid deck."),
-            LorError::Version => write!(
-                f,
-                "The provided code requires a higher version of this library; please update."
-            ),
-        }
-    }
-}
-
-impl Error for LorError {
-    fn description(&self) -> &str {
-        match *self {
-            LorError::Decode(ref err) => err.description(),
-            LorError::VarintDecode(ref err) => err.description(),
-            LorError::InvalidCardCode(ref err) => err.description(),
-            LorError::InvalidCard => "invalid card error",
-            LorError::InvalidDeck => "invalid deck error",
-            LorError::Version => "version error",
-        }
-    }
-}
-
-impl From<data_encoding::DecodeError> for LorError {
-    fn from(err: data_encoding::DecodeError) -> Self {
-        LorError::Decode(err)
-    }
-}
-
-impl From<std::io::Error> for LorError {
-    fn from(err: std::io::Error) -> Self {
-        LorError::VarintDecode(err)
-    }
-}
-
-impl From<std::num::ParseIntError> for LorError {
-    fn from(err: std::num::ParseIntError) -> Self {
-        LorError::InvalidCardCode(err)
-    }
 }
